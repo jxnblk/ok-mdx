e78c2f1687862b5f64c3408ee7e82649236ddd38
Brent Jackson <jxnblk@gmail.com>
2018-07-28 20:55:52 -0400

    Adjust module

--- packages/ok-cli/index.js
+++ packages/ok-cli/index.js
@@ -1,4 +1,3 @@
 module.exports = require('./lib/index')
 module.exports.config = require('./lib/index').config
 module.exports.build = require('./lib/build')
-module.exports.overlay = require('./lib/overlay')
