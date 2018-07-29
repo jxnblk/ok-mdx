4308579e01a68d14c19473fba8152e0dc2d791fb
Brent Jackson <jxnblk@gmail.com>
2018-07-28 20:51:15 -0400

    ok-cli: edit module

---
+++ packages/ok-cli/index.js
@@ -0,0 +1,4 @@
+module.exports = require('./lib/index')
+module.exports.config = require('./lib/index').config
+module.exports.build = require('./lib/build')
+module.exports.overlay = require('./lib/overlay')
