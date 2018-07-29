43339de896a768052cdd2871c3307211bdd29fdd
Brent Jackson <jxnblk@gmail.com>
2018-07-28 20:18:38 -0400

    ok-cli: remove components default

--- packages/ok-cli/lib/entry.js
+++ packages/ok-cli/lib/entry.js
@@ -3,7 +3,7 @@ import { render } from 'react-dom'
 
 const mod = require(APP_FILENAME)
 const App = mod.default
-const { components = {} } = mod
+const { components } = mod
 render(<App components={components} />, window.root)
 
 if (module.hot) module.hot.accept()
